792ef8fe5d26b4605de1caf0c37d65a9270fb523
Cristiano Maia <criscmaia@gmail.com>
2016-11-14 12:38:03 +0000

    Map the data submitted from the PHP form
     Further explanation
    This is the result of a POST request when the PHP form is submitted:
    
    "        
    POST /user.php HTTP/1.1
    Host: localhost
    User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)
    Accept: text/html,application/xhtml+xml,application/xml;q=0.9,
    Accept-Language: en-us,en;q=0.5
    Accept-Encoding: gzip,deflate
    Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7
    Keep-Alive: 300
    Connection: keep-alive
    Referer: http://localhost/test.php
    Content-Type: application/x-www-form-urlencoded
    Content-Length: 43
     
    first_name=John&last_name=Doe&email=j.doe@mdx.ac.uk&action=Submit
    "
            
    The last line is the BODY of the POST Response and what is being saved in the map variable "postResponseMapped"
    
    Of course, it needs to be tested.

--- src/main/java/HTTPServer.java
+++ src/main/java/HTTPServer.java
@@ -1,5 +1,6 @@
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.Headers;      // used to get the POST values from the Header body
 import com.ullink.slack.simpleslackapi.SlackSession;
 import com.ullink.slack.simpleslackapi.replies.ParsedSlackReply;
 
@@ -17,9 +18,17 @@ public class HTTPServer implements HttpHandler {
 
     public void handle(HttpExchange t) throws IOException {
 
-        String response = t.getRequestURI().getPath();
-        String[] userDetails = response.split("/");
-        System.out.println(response);
+        // https://docs.oracle.com/javase/8/docs/jre/api/net/httpserver/spec/com/sun/net/httpserver/Headers.html
+        String postResponse = t.getRequestHeaders();
+        Set<Map.Entry<String, List<String>>> postResponseMapped = headers.entrySet();
+        // it gets the reader values and save them in a Map variable above
+        // now you read directly from the original URL (/user.php) and use port 443
+         
+        
+        // -- can't track the URL because it won't show anything there
+        // String response = t.getRequestURI().getPath();
+        // String[] userDetails = response.split("/");
+        // System.out.println(response);
         ParsedSlackReply reply = session.inviteUser(userDetails[1],userDetails[2],true).getReply();
 
         t.sendResponseHeaders(202, response.length());
